03c6ce02a7726ab3cd9141f8352e47eb77e74131
Caroline Miehe <caroline.miehe@kuleuven.be>
2020-03-30 17:15:15 +0200

    first day of R

--- sc_report/analysis.R
+++ sc_report/analysis.R
@@ -1,4 +1,7 @@
 rm(list=ls())
+#remove and rm can be used to remove objects
+#list: a character vector naming objects to be removed
+#ls shows what data sets and functions a user has defined
 
 if (Sys.info()['sysname'] =="Windows") {
 path <- "C:/users/u0127963/Desktop/PhD/baraza"
@@ -47,3 +50,137 @@ t.test(sc_endline_no_dist$baraza.km.D1~sc_endline_no_dist$ind_treat, var.equal=T
 ## run a regression
 summary(lm(baraza.km.D1~ind_treat, data = sc_endline_no_dist))
 
+###Caroline's first R day:
+#####C1#####
+sc_endline_no_dist$baraza.visit_times.C1
+hist(sc_endline_no_dist$baraza.visit_times.C1)
+summary(sc_endline_no_dist$baraza.visit_times.C1)
+#is 48 an outlier?
+boxplot(sc_endline_no_dist$baraza.visit_times.C1)
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C1, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#na.rm=T is Excluding Missing Values from Analyses
+#difference significant? t-test
+t.test(sc_endline_no_dist$baraza.visit_times.C1~sc_endline_no_dist$ind_treat, var.equal=T)
+#regression
+summary(lm(baraza.visit_times.C1~ind_treat, data = sc_endline_no_dist))
+#lm means linear model
+
+#####C2#####
+sc_endline_no_dist$baraza.visit_times.C2
+#replace 999s by NA
+sc_endline_no_dist$baraza.visit_times.C2[ sc_endline_no_dist$baraza.visit_times.C2 == 999 ] <- NA
+hist(sc_endline_no_dist$baraza.visit_times.C2)
+summary(sc_endline_no_dist$baraza.visit_times.C2)
+boxplot(sc_endline_no_dist$baraza.visit_times.C2)
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C2, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#na.rm=T is Excluding Missing Values from Analyses
+#difference significant? t-test
+t.test(sc_endline_no_dist$baraza.visit_times.C2~sc_endline_no_dist$ind_treat, var.equal=T)
+#regression
+summary(lm(baraza.visit_times.C2~ind_treat, data = sc_endline_no_dist))
+#lm means linear model
+
+###C3###
+sc_endline_no_dist$baraza.visit_times.C3
+#replace 999s by NA
+sc_endline_no_dist$baraza.visit_times.C3[ sc_endline_no_dist$baraza.visit_times.C3 == 999 ] <- NA
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C3, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C3~ind_treat, data = sc_endline_no_dist))
+
+###C4###
+sc_endline_no_dist$baraza.visit_times.C4
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C4, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C4~ind_treat, data = sc_endline_no_dist))
+
+###C5###
+sc_endline_no_dist$baraza.visit_times.C5
+#replace 999s by NA
+sc_endline_no_dist$baraza.visit_times.C5[ sc_endline_no_dist$baraza.visit_times.C5 == 999 ] <- NA
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C5, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C5~ind_treat, data = sc_endline_no_dist))
+
+###C6###
+sc_endline_no_dist$baraza.visit_times.C6
+#replace 999s by NA
+sc_endline_no_dist$baraza.visit_times.C6[ sc_endline_no_dist$baraza.visit_times.C6 == 999 ] <- NA
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C6, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C6~ind_treat, data = sc_endline_no_dist))
+
+###C7###
+sc_endline_no_dist$baraza.visit_times.C7
+#replace 999s by NA
+sc_endline_no_dist$baraza.visit_times.C7[ sc_endline_no_dist$baraza.visit_times.C7 == 999 ] <- NA
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C7, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C7~ind_treat, data = sc_endline_no_dist))
+
+###C8###
+sc_endline_no_dist$baraza.visit_times.C8
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C8, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C8~ind_treat, data = sc_endline_no_dist))
+
+###C9###
+sc_endline_no_dist$baraza.visit_times.C9
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.visit_times.C9, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.visit_times.C9~ind_treat, data = sc_endline_no_dist))
+
+###D1###
+sc_endline_no_dist$baraza.km.D1
+#replace 999s by NA
+sc_endline_no_dist$baraza.km.D1[ sc_endline_no_dist$baraza.km.D1 == 999 ] <- NA
+#means by treatment group
+tapply(sc_endline_no_dist$baraza.km.D1, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+#regression
+summary(lm(baraza.km.D1~ind_treat, data = sc_endline_no_dist))
+
+###D2-4b###
+sc_endline_no_dist$baraza.km.D2
+sc_endline_no_dist$baraza.km.D2[ sc_endline_no_dist$baraza.km.D2 == 999 ] <- NA
+sc_endline_no_dist$baraza.km.D3
+sc_endline_no_dist$baraza.km.D3[ sc_endline_no_dist$baraza.km.D3 == 999 ] <- NA
+sc_endline_no_dist$baraza.km.D4a
+sc_endline_no_dist$baraza.km.D4a[ sc_endline_no_dist$baraza.km.D4a == 999 ] <- NA
+sc_endline_no_dist$baraza.km.D4b
+sc_endline_no_dist$baraza.km.D4b[ sc_endline_no_dist$baraza.km.D4b == 999 ] <- NA
+#regression
+summary(lm(baraza.km.D2~ind_treat, data = sc_endline_no_dist))
+summary(lm(baraza.km.D3~ind_treat, data = sc_endline_no_dist))
+summary(lm(baraza.km.D4a~ind_treat, data = sc_endline_no_dist))
+tapply(sc_endline_no_dist$baraza.km.D4a, sc_endline_no_dist$ind_treat, FUN=mean, na.rm=T)
+t.test(sc_endline_no_dist$baraza.km.D4a~sc_endline_no_dist$ind_treat, var.equal=T)
+
+###E1a-e7###
+sc_endline_no_dist$baraza.production.E1a
+summary(lm(baraza.production.E1a~ind_treat, data = sc_endline_no_dist))
+sc_endline_no_dist$baraza.health.E2a
+summary(lm(baraza.health.E2a~ind_treat, data = sc_endline_no_dist))
+sc_endline_no_dist$baraza.gender1.E3a
+summary(lm(baraza.gender1.E3a~ind_treat, data = sc_endline_no_dist))
+sc_endline_no_dist$baraza.works.E4a
+summary(lm(baraza.works.E4a~ind_treat, data = sc_endline_no_dist))
+sc_endline_no_dist$baraza.finance1.E5a
+summary(lm(baraza.finance1.E5a~ind_treat, data = sc_endline_no_dist))
+#how to measure difference date and today
+sc_endline_no_dist$baraza.finance1.E5a
+summary(lm(baraza.finance1.E5a~ind_treat, data = sc_endline_no_dist))
+
+########Caroline's first R day, after call at 4pm
+#sc, information and deliberation
+ols <- lm( baraza.H1~ information * deliberation + region, data = sc_endline[sc_endline$district_baraza == 0,] )
+summary(ols)
+coef(ols)
